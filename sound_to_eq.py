565ee63555f1921ad445947664ac228d5edd8264
cmnsooo <166024217+cmnsooo@users.noreply.github.com>
2025-02-26 00:06:12 +0900

    Update sound_to_eq.py

--- sound_to_eq.py
+++ sound_to_eq.py
@@ -3,10 +3,7 @@ from sound_input import record_audio
 from detect_earthquake import detecting, findEq
 
 def main():
-    # 실시간으로 오디오 녹음
     audio_data, fs = record_audio()
-
-    # 16000Hz 주파수를 시각화
     processed_data = visualize(audio_data, fs, target_freq=100)
     raw, trig, char = detecting(processed_data)
     findEq(raw, trig, char)
